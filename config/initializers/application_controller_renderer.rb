7b2c74944e348ca339d51c531a03b2aa146b2288
Nathalie <125998306+fillonNat@users.noreply.github.com>
2023-05-24 16:52:33 +0000

    changement version et boostrap ajouté

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
+# config/initializers/activerecord_yaml.rb
+
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
